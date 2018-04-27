eec53c48822d918b47cb805219151175ad282b8f
Renée Kooi <renee@kooi.me>
2018-04-27 17:47:30 +0200

    meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -27,6 +27,10 @@ export default class extends Document {
         <Head>
           <style dangerouslySetInnerHTML={{ __html: 'body { margin: 64px 0 0 0; font-family: "Open Sans", sans-serif; }' }} />
           <style id="jss" dangerouslySetInnerHTML={{ __html: styles }} />
+          <title>üWave</title>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="üWave is a self-hosted collaborative listening platform." />
         </Head>
         <body>
           <Main />
