76dca74801d32c83fee950f726314fcd60dd10b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-06 14:54:06 +0000

    Deploy http://u-wave.net to github.com/u-wave/u-wave.github.io.git:master

---
+++ _next/static/JDd_z0MrFpvVrcq3NptOv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/install":[a]}}("static\u002Fchunks\u002F4c070a5df94d9f52416803e2d2cb3ae76003c5fb.1301c093eade8eb6c9f1.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
