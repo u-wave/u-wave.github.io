5446ac57a15daf22edc4a3648bdb0fa71796d720
Renée Kooi <renee@kooi.me>
2020-08-04 10:59:24 +0200

    npm update

--- layout/rollup.config.js
+++ layout/rollup.config.js
@@ -1,17 +1,19 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 
 const pkg = require('./package.json');
 
 export default {
   input: './src/withLayout.js',
   output: [
-    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'default' },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   external(id) {
     return id === 'react' || id.startsWith('@material-ui');
   },
   plugins: [
-    babel(),
+    babel({
+      runtimeHelpers: 'bundled',
+    }),
   ],
 };
